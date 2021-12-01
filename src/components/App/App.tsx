ee86bbb502eba1497e6ada3cceefaa748c0c5f0c
javillegas2 <javillegas162@gmail.com>
2021-11-30 20:39:52 -0500

    Subtitulo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { Header } from "../Header";
 import { MainBanner } from "../MainBanner";
+import { Subtitle } from "../Subtitle";
 
 
 
@@ -9,6 +10,7 @@ export const App = (): ReactElement | null => {
     <div className="App">
       <Header />
       <MainBanner/>
+      <Subtitle />
       
     </div>
   );
---
+++ src/components/Subtitle/Subtitle.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactElement } from "react";
+
+import './Subtitle.scss';
+
+
+export const Subtitle: FC = (): ReactElement | null => {
+  return (
+      <div className="container-subtitle">
+          Nuevas Recetas
+
+      </div>
+    
+  );
+};
+
+
