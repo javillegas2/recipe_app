0fc1662634fb9ddf493bf9059b4fb9fccf849276
javillegas2 <javillegas162@gmail.com>
2021-12-01 09:31:01 -0500

    Cargando de un arreglo los titulos de las recetas

--- src/components/ContainerCards/Card.tsx
+++ src/components/ContainerCards/Card.tsx
@@ -8,9 +8,15 @@ export const Card: FC<cardT> = ({img, title, subtitle }): ReactElement | null =>
   return (
       <div className="card">
           <div className="card-header">
-              <div className = "header-image" style={{ backgroundImage: "url('../../img/Ojingeo-muchim.png')"}}></div>
+              <div className = "header-image" style={{ backgroundImage: 'url(../../img/'+img+')'}}></div>
           </div>
-          <div className="card-body"></div>
+          <div className="card-body">
+              <div className="empty-space-sup"></div>
+              <div className="card-names">
+                  <span className="title">{title}</span>
+                  <span className="subtitle">{subtitle}</span>
+              </div>
+          </div>
 
           
 
--- src/components/ContainerCards/ContainerCards.tsx
+++ src/components/ContainerCards/ContainerCards.tsx
@@ -4,17 +4,41 @@ import { Card } from "./Card";
 import './ContainerCards.scss';
 
 
-export const ContainerCards: FC = (): ReactElement | null => {
-  return (
-      <div className="container-cards">
-          <Card img={'Ojingeo-muchim.png'} title ={'Ojinjeo'} subtitle ={'Muchim'} />
-          <Card img={'Ojingeo-muchim.png'} title ={'Ojinjeo'} subtitle ={'Muchim'} />
-          <Card img={'Ojingeo-muchim.png'} title ={'Ojinjeo'} subtitle ={'Muchim'} />
-          <Card img={'Ojingeo-muchim.png'} title ={'Ojinjeo'} subtitle ={'Muchim'} />
-          
+const recipes = [
+  {
+    img: 'Ojingeo-muchim-5.png',
+    title: 'Ojinjeo',
+    subtitle: 'Muchim'
+  },
+  {
+    img: 'Ojingeo-muchim.png',
+    title: 'Cola',
+    subtitle: 'Chicken'
+  },
+  {
+    img: 'Ojingeo-muchim-1.png',
+    title: 'Roasted',
+    subtitle: 'Carrot'
+  },
+  {
+    img: 'cherry.png',
+    title: 'Sweet',
+    subtitle: 'Cherries'
+  }
 
-      </div>
-    
+];
+
+export const ContainerCards: FC = (): ReactElement | null => {
+
+
+  return (
+    <div className="container-cards">
+
+      {recipes.map(item => {
+        return <Card img={item.img} title={item.title} subtitle={item.subtitle} />
+      })}
+    </div>
+
   );
 };
 
